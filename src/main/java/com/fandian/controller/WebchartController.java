aa00e0f944984d40b6917a4d5d4d399011210a92
ganhaitian <ganhaitian@gmail.com>
2014-07-14 23:57:10 +0800

    添加微信接入接口

---
+++ src/main/java/com/fandian/controller/WebchartController.java
@@ -0,0 +1,24 @@
+package com.fandian.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ganhaitian
+ * Date: 14-7-14
+ * Time: 下午11:30
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class WebchartController {
+
+    @RequestMapping(value="/testEcho",method= RequestMethod.GET)
+    @ResponseBody
+    public String testEcho(@RequestParam String signature,@RequestParam String timestamp,@RequestParam String nonce,@RequestParam String echostr){
+        return echostr;
+    }
+
+
+
+}
