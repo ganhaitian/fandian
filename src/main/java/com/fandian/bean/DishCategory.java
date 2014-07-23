252d7eaad9f275cb646c26240dd7b26653a53bdb
meajhlp <meajhlp@gmail.com>
2014-07-23 21:53:33 +0800

    菜品分类增加多级

--- src/main/java/com/fandian/bean/DishCategory.java
+++ src/main/java/com/fandian/bean/DishCategory.java
@@ -1,5 +1,7 @@
 package com.fandian.bean;
 
+import java.util.List;
+
 /**
  * Created by gan on 14-7-21.
  */
@@ -8,6 +10,12 @@ public class DishCategory {
     private int id;
 
     private String name;
+
+    private int parent_id;
+
+    private List<DishCategory> childCategories;
+
+
 
     public int getId() {
         return id;
@@ -23,5 +31,21 @@ public class DishCategory {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getParent_id() {
+        return parent_id;
+    }
+
+    public void setParent_id(int parent_id) {
+        this.parent_id = parent_id;
+    }
+
+    public List<DishCategory> getChildCategories() {
+        return childCategories;
+    }
+
+    public void setChildCategories(List<DishCategory> childCategories) {
+        this.childCategories = childCategories;
     }
 }
